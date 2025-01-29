b2d7ab403a55d752bf1c85c9af903f5d9848140d
Arka Kundu <arka99325@gmail.com>
2025-01-29 22:00:38 +0530

    Time: 3 ms (41.99%), Space: 41.6 MB (81.76%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] ch = s.toCharArray();
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (ch[i] == '[' || ch[i] == '{' || ch[i] == '(') {
+                st.push(ch[i]);
+            }
+            else {
+                if (st.isEmpty()) return false;
+                char top = st.peek();
+                if ((ch[i] == ')' && top == '(') ||
+                    (ch[i] == '}' && top == '{') ||
+                    (ch[i] == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false; // Mismatched or invalid character
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
