ed5d2185d391478d62f6f5207193d4b127996492
Arka Kundu <arka99325@gmail.com>
2025-02-06 22:19:41 +0530

    Time: 8 ms (89.81%), Space: 44 MB (82.42%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+       if(n <= 0) return false;
+       double ans = Math.log(n)/Math.log(3);
+       return Math.abs(ans - Math.round(ans)) < 1e-10;
+    }
+}
