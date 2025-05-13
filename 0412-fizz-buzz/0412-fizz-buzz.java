75fdbce183afe06018556610ff297b8e1c80a958
Arka Kundu <arka99325@gmail.com>
2025-05-13 08:18:26 +0530

    Time: 1 ms (99.82%), Space: 45.3 MB (41.66%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        int i = 1;
+        while(i <= n){
+            if(i % 3 == 0 && i % 5 == 0) list.add("FizzBuzz");
+            else if(i % 5 == 0) list.add("Buzz");
+            else if(i % 3 == 0 ) list.add("Fizz");
+            else list.add(i+"");
+            i++;
+        }
+        return list;
+    }
+}
