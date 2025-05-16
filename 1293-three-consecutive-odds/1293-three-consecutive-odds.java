f6ef5fc748e71db5d66ea5fec01e135064e2117e
Arka Kundu <arka99325@gmail.com>
2025-05-16 11:52:29 +0530

    Time: 0 ms (100%), Space: 42.6 MB (26.49%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+     int count = 0;    
+     for(int c : arr){
+        if(c % 2 != 0){
+            count++;
+            if(count == 3) return true;
+        }
+        else count = 0;
+     }
+         return false;   
+    }
+}
