3c4ea89643a4fb5dae07de01cc7d29a42879a6be
Arka Kundu <arka99325@gmail.com>
2025-07-24 10:16:43 +0530

    Time: 0 ms (100%), Space: 41.4 MB (76.85%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,7 +5,7 @@ class Solution {
         String prefix = strs[0];
         for(int i = 1; i < n; i++){
           while(strs[i].indexOf(prefix) != 0){
-            prefix = prefix.substring(0, prefix.length() -1);
+            prefix = prefix.substring(0, prefix.length()-1);
             if(prefix.isEmpty()) return "";
           }
         }
