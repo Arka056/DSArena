b22ca5cfef212130c303494873510457d6ea23d8
Arka Kundu <arka99325@gmail.com>
2025-07-27 12:50:02 +0530

    Time: 0 ms (100%), Space: 41.6 MB (65.43%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,16 +14,42 @@
  * }
  */
 class Solution {
-    public void inOrder(TreeNode temp, List<Integer> a){
-        if(temp != null){
-            inOrder(temp.left, a);
-            a.add(temp.val);
-            inOrder(temp.right, a);
-        }
-    }
+    // public void inOrder(TreeNode temp, List<Integer> a){
+    //     if(temp != null){
+    //         inOrder(temp.left, a);
+    //         a.add(temp.val);
+    //         inOrder(temp.right, a);
+    //     }
+    // }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> arr = new ArrayList<>();
-        inOrder(root, arr);
+        // inOrder(root, arr);
+        // return arr;
+        TreeNode curr = root;
+        while(curr != null){
+            // printing current node
+            if(curr.left == null){
+                arr.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                // Finding Inorder Predecessor
+                TreeNode p = curr.left;
+                while(p.right != null && p.right != curr){
+                    p = p.right;
+                }
+                // Creating the Morris Thread
+                if(p.right == null){
+                    p.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    p.right = null; // Manually Thread is deleted
+                    arr.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
         return arr;
     }
 }
