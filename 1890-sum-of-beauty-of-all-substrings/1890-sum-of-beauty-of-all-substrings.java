d37739f5e5bdc199c204e3e94662340cbfd405b3
Arka Kundu <arka99325@gmail.com>
2025-08-11 09:44:08 +0530

    Time: 57 ms (71.38%), Space: 42.9 MB (59.91%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int beautySum(String s) {
+       int sum = 0;
+       for(int i = 0; i < s.length(); i++){
+        int []freq = new int[26];
+        for(int j = i; j < s.length(); j++){
+            int maxFreq = 0;
+            int minFreq = Integer.MAX_VALUE;
+            freq[s.charAt(j) - 'a']++;
+            for(int f : freq){
+                if(f > 0){
+                    maxFreq = Math.max(maxFreq, f);
+                    minFreq = Math.min(minFreq, f);
+                }
+            }
+            sum += (maxFreq - minFreq);
+        }
+       }
+       return sum;
+    }
+}
