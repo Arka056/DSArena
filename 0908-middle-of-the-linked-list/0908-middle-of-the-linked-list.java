2700f7dac3bd25b619f94b0d5f66e1e25c55ccc3
Arka Kundu <arka99325@gmail.com>
2025-08-22 02:02:37 +0530

    Time: 0 ms (100%), Space: 41.1 MB (56.85%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -9,16 +9,14 @@
  * }
  */
 class Solution {
-    public ListNode middleNode(ListNode head) 
-    {
-        // Tortoise method
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null)return head;
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next != null)
-            {
-                slow = slow.next;
-                fast = fast.next.next;
-            }
-            return slow;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast =fast.next.next;
         }
+        return slow;
     }
+}
