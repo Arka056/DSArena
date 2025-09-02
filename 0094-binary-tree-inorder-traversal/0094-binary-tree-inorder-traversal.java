560e904ef11320e03f984510a6d1abceb831df83
Arka Kundu <arka99325@gmail.com>
2025-09-02 17:39:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (8.31%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -25,29 +25,44 @@ class Solution {
         List<Integer> arr = new ArrayList<>();
         // inOrder(root, arr);
         // return arr;
-        TreeNode curr = root;
-        while(curr != null){
-            // printing current node
-            if(curr.left == null){
-                arr.add(curr.val);
-                curr = curr.right;
+        // TreeNode curr = root;
+        // while(curr != null){
+        //     // printing current node
+        //     if(curr.left == null){
+        //         arr.add(curr.val);
+        //         curr = curr.right;
+        //     }
+        //     else{
+        //         // Finding Inorder Predecessor
+        //         TreeNode p = curr.left;
+        //         while(p.right != null && p.right != curr){
+        //             p = p.right;
+        //         }
+        //         // Creating the Morris Thread
+        //         if(p.right == null){
+        //             p.right = curr;
+        //             curr = curr.left;
+        //         }
+        //         else{
+        //             p.right = null; // Manually Thread is deleted
+        //             arr.add(curr.val);
+        //             curr = curr.right;
+        //         }
+        //     }
+        // }
+        // return arr;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+        while(true){
+            if(node != null){
+                st.push(node);
+                node = node.left;
             }
             else{
-                // Finding Inorder Predecessor
-                TreeNode p = curr.left;
-                while(p.right != null && p.right != curr){
-                    p = p.right;
-                }
-                // Creating the Morris Thread
-                if(p.right == null){
-                    p.right = curr;
-                    curr = curr.left;
-                }
-                else{
-                    p.right = null; // Manually Thread is deleted
-                    arr.add(curr.val);
-                    curr = curr.right;
-                }
+                if(st.isEmpty())break;
+                node = st.pop();
+                arr.add(node.val);
+                node = node.right;
             }
         }
         return arr;
