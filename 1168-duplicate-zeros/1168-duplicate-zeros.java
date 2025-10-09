74cf7a6c5700bd38773a4b84457bbd0a7b3d4d18
Arka Kundu <arka99325@gmail.com>
2025-10-09 10:42:39 +0530

    Time: 0 ms (100%), Space: 45.3 MB (6.55%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,16 +1,33 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        Queue<Integer> q = new LinkedList<>();
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i = 0; i < arr.length; i++){
+        //     if(arr[i] == 0){
+        //         q.add(0);
+        //         q.add(0);
+        //     }
+        //     else{
+        //         q.add(arr[i]);
+        //     }
+        //     int f = q.poll();
+        //     arr[i] = f;
+        // }
+
+        int [] res = arr.clone();
+        int j = 0;
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 0){
-                q.add(0);
-                q.add(0);
+            if(res[j]==0){
+                arr[i] = 0;
+                if(i+1 < arr.length){
+                    arr[i+1] = 0;
+                    i++;
+                    j++;
+                }
             }
             else{
-                q.add(arr[i]);
+                arr[i] = res[j];
+                j++;
             }
-            int f = q.poll();
-            arr[i] = f;
         }
     }
 }
