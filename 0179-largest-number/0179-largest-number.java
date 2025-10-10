1973691bc25601cc5d5270b242c9398068526e8c
Arka Kundu <arka99325@gmail.com>
2025-10-10 23:18:19 +0530

    Time: 5 ms (98.23%), Space: 43.4 MB (40.7%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String [] s = new String[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            s[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(s, (a,b) -> (b+a).compareTo(a+b));
+        StringBuilder sb = new StringBuilder();
+        for(String s1: s){
+            sb.append(s1);
+        }
+        return sb.charAt(0) == '0' ? "0": sb.toString();
+    }
+}
