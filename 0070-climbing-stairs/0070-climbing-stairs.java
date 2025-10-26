1758143da1c3d3649fe1bfb287f78625826d6e8a
Arka Kundu <arka99325@gmail.com>
2025-10-26 19:16:57 +0530

    Time: 0 ms (100%), Space: 40.4 MB (67.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        int []dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        // int []dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // dp[0]=1;
+        // dp[1]=1;
+        // for(int i = 2; i <= n; i++){
+        //     dp[i] = dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
+        int i = 1;
+        int j = 1;
+        int curr;
+        for(int t =2; t <= n; t++){
+            curr = i+j;
+            i = j;
+            j = curr;
         }
-        return dp[n];
+        return j;
     }
 }
