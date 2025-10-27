589c333c6150b89705b452901458fb5adc4dca58
Arka Kundu <arka99325@gmail.com>
2025-10-27 23:15:11 +0530

    Time: 1 ms (78.72%), Space: 45.7 MB (86.16%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public int maxProfit(int[] p) {
         int maxP = 0;
         for(int i = 1; i < p.length; i++){
+            // Checking if the next day price is better, we sell
             if(p[i] > p[i-1]){
                 maxP += p[i] - p[i-1];
             }
