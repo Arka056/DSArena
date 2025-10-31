94135f47d58bc45010a95bc3fc1d3cf07920c6d0
Arka Kundu <arka99325@gmail.com>
2025-10-31 11:04:50 +0530

    Time: 0 ms (100%), Space: 42.1 MB (9.11%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -0,0 +1,10 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        boolean [] freq = new boolean[26];
+        for(char c: s.toCharArray()){
+            if(freq[c-'a']) return c;
+            freq[c-'a'] = true;
+        }
+        return ' ';
+    }
+}
